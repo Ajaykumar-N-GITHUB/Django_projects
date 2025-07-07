e2d7f5926bf197701b9f1f72ee216e81fa4c19ab
anattudu <anattudu@cisco.com>
2025-07-07 13:55:03 +0530

    latetst push-2 - 7th july

--- ngo_mailsender/ngo_mailsender/settings.py
+++ ngo_mailsender/ngo_mailsender/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 import os
 from pathlib import Path
-
+import dj_database_url
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -78,15 +78,23 @@ WSGI_APPLICATION = 'ngo_mailsender.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'ardsidb',
+#         'USER': 'ardsi',
+#         'PASSWORD': 'ardsi@2025',
+#         'HOST': 'localhost',
+#         'PORT': '5432',
+#     }
+# }
+
+
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'ardsidb',
-        'USER': 'ardsi',
-        'PASSWORD': 'ardsi@2025',
-        'HOST': 'localhost',
-        'PORT': '5432',
-    }
+    'default': dj_database_url.parse(
+        os.environ.get("DATABASE_URL")
+    )
 }
 
 
